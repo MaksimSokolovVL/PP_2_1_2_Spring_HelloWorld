9928db79c146f33420dbd2d78c0b9c24ed415f1a
MaksimSokolovVL <volokos2010@gmail.com>
2023-03-05 23:04:16 +0300

    Spring "Hello World"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHW =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2HW =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHW.getMessage());
+        System.out.println(beanHW == bean2HW);
+
+
+        Cat beanCat = (Cat) applicationContext.getBean("getCat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("getCat");
+
+        System.out.println(beanCat == beanCat2);
+
+
+
     }
 }
